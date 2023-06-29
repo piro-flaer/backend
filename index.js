b863fdaf0ec3d3fae32490ab0e075c06a59ee0a3
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-06-29 17:45:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const logEvents = require("./middleware/logger");
+
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+
+const dbConnect = require("./config/dbConn");
+const mongoose = require("mongoose");
+
+cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+const PORT = 3500;
+
+dbConnect();
+
+app.use((req, res, next) => {
+  logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`);
+  next();
+});
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use("/", require("./routes/root"));
+
+app.use("/trekList", require("./routes/trekListRoute"));
+
+app.use("/auth", require("./routes/authRoute"));
+
+app.use("/profile", require("./routes/profileRoute"));
+
+app.use("/addFavorite", require("./routes/addFavoriteRoute"));
+
+app.all("*", (req, res) => {
+  res.sendStatus(404);
+});
+
+mongoose.connection.once("open", () => {
+  logEvents("Connected to MongoDB");
+
+  app.listen(PORT, () => {
+    logEvents(`Server running on port ${PORT}`);
+    console.log(`Server Started`);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  logEvents(`${err}`);
+});
