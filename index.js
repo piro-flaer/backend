112ee467209f35ebec7e7d515ccdfa454513477f
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 18:38:13 +0530

    before jwt

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use((req, res, next) => {
 
 app.use(cors(corsOptions));
 
+app.use(express.json({ limit: "50mb" }));
 app.use(express.json());
 
 app.use("/", require("./routes/root"));
@@ -34,7 +35,9 @@ app.use("/auth", require("./routes/authRoute"));
 
 app.use("/profile", require("./routes/profileRoute"));
 
-app.use("/addFavorite", require("./routes/addFavoriteRoute"));
+app.use("/preferences", require("./routes/preferencesRoute"));
+
+app.use("/favorite", require("./routes/favoriteRoute"));
 
 app.all("*", (req, res) => {
   res.sendStatus(404);
