54584ba8186e46a5290595bdef4be28dc6bd36e2
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 17:42:24 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,9 @@
 const allowedOrigins = [
-    'http://localhost:3100',
-    'ThunderClient'
-]
+  "http://localhost:3100",
+  "52.41.36.82",
+  "54.191.253.12",
+  "44.226.122.3",
+  "ThunderClient",
+];
 
-module.exports = allowedOrigins
+module.exports = allowedOrigins;
