e2cf4a7359da3fab164d3ccf4b15c5c510ca0919
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 17:48:45 +0530

    Create updatePreferences.js

---
+++ controllers/updatePreferences.js
@@ -0,0 +1,28 @@
+const userPreferencesModel = require("../models/userPreferencesModel");
+
+const bcrypt = require("bcryptjs");
+
+const logEvents = require("../middleware/logger");
+
+const updatePreferences = async (req, res) => {
+  const { userName, state, season, difficulty } = req.body;
+
+  const userPreferencesObject = await userPreferencesModel.findOne({
+    userName,
+  });
+
+  userPreferencesObject.state = state;
+  userPreferencesObject.season = season;
+  userPreferencesObject.difficulty = difficulty;
+
+  const userPreferencesUpdated = await userPreferencesObject.save();
+
+  if (userPreferencesUpdated) {
+    logEvents(`Preferences updated`);
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(400);
+  }
+};
+
+module.exports = updatePreferences;
