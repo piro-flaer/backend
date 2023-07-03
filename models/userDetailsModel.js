0869cc33915ecfe072b75c490c1b468434063ce3
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 17:52:24 +0530

    Update userDetailsModel.js

--- models/userDetailsModel.js
+++ models/userDetailsModel.js
@@ -1,23 +1,29 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const userDetailsSchema = new mongoose.Schema({
+const userDetailsSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     lastName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     userName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true
-    }
-},
-  { collection: 'userDetails' })
+      type: String,
+      required: true,
+    },
+    profile: {
+      type: String,
+      required: true,
+    },
+  },
+  { collection: "userDetails" }
+);
 
-module.exports = mongoose.model('userDetailsModel', userDetailsSchema)
+module.exports = mongoose.model("userDetailsModel", userDetailsSchema);
