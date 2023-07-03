683f6980f5989b75e96c552c371578ff53afdcf3
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 17:45:09 +0530

    Create getFavorite.js

---
+++ controllers/getFavorite.js
@@ -0,0 +1,17 @@
+const userFavoritesModel = require("../models/userFavoritesModel");
+const trekDetailsModel = require("../models/trekDetailsModel");
+
+const getFavorite = async (req, res) => {
+  const { userName } = req.query;
+  const userFavorites = await userFavoritesModel.findOne({ userName }).lean();
+
+  const trekList = await trekDetailsModel.find().lean();
+
+  const userFavoritesTreks = trekList.filter((trek) =>
+    userFavorites.favorites.includes(trek.name)
+  );
+
+  res.status(200).json(userFavoritesTreks);
+};
+
+module.exports = getFavorite;
