4941a11ddc3ea38d03400d1a396f0cddb749339b
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 17:43:57 +0530

    Update createUser.js

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -1,46 +1,37 @@
 const userDetailsModel = require("../models/userDetailsModel");
 const userIDModel = require("../models/userIDModel");
-const userPreferencesModel = require("../models/userPreferencesModel");
 
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const logEvents = require("../middleware/logger");
 
 const createUser = async (req, res) => {
-  const {
-    firstName,
-    lastName,
-    userName,
-    email,
-    password,
-    difficulty,
-    state,
-    season,
-  } = req.body;
+  const { firstName, lastName, userName, email, password, profile } = req.body;
 
-  const duplicate = await userIDModel.findOne({ userName }).lean();
-  if (duplicate) {
+  const duplicateUserName = await userIDModel.findOne({ userName }).lean();
+  if (duplicateUserName) {
     logEvents(`User not created: Username already exists!`);
-    return res.status(409).json({ message: "Username already exists!" });
+    return res.status(409).json({ message: "UserName Already Exists!" });
+  }
+  const duplicateMail = await userIDModel.findOne({ email }).lean();
+  if (duplicateMail) {
+    logEvents(`User not created: EMail already exists!`);
+    return res.status(409).json({ message: "E Mail Already Exists!" });
   }
 
   const hashedPsswrd = await bcrypt.hash(password, 10);
 
-  const userDetailsObject = { firstName, lastName, userName, email };
+  const userDetailsObject = { firstName, lastName, userName, email, profile };
   const userIDObject = { userName, password: hashedPsswrd };
-  const userPreferencesObject = { userName, difficulty, state, season };
 
   const userDetailCreated = await userDetailsModel.create(userDetailsObject);
   const userIDCreated = await userIDModel.create(userIDObject);
-  const userPreferencesCreated = await userPreferencesModel.create(
-    userPreferencesObject
-  );
 
-  if (userDetailCreated && userIDCreated && userPreferencesCreated) {
+  if (userDetailCreated && userIDCreated) {
     logEvents(`User created`);
-    res.status(201).json({ message: `User created` });
+    res.sendStatus(201);
   } else {
-    res.status(400).json({ message: "User not created" });
+    res.sendStatus(400);
   }
 };
 
