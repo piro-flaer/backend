4344996053f1249a4282520e323c4da1e7534597
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-23 13:31:24 +0530

    Update profileRoute.js

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const getUserDetails = require("../controllers/getUserDetails");
-const createUser = require("../controllers/createUser");
 const updateUserDetails = require("../controllers/updateUserDetails");
 
-router.route("/").get(getUserDetails).post(createUser).put(updateUserDetails);
+router.route("/").get(getUserDetails).put(updateUserDetails);
 
 module.exports = router;
