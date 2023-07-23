d199caab6faa3173cbb7f4e96ad87c7a0c597bdd
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-23 13:23:37 +0530

    Update createUser.js

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -1,6 +1,8 @@
 const userDetailsModel = require("../models/userDetailsModel");
 const userIDModel = require("../models/userIDModel");
+const userFavoritesModel = require("../models/userFavoritesModel");
 
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 const logEvents = require("../middleware/logger");
@@ -23,13 +25,42 @@ const createUser = async (req, res) => {
 
   const userDetailsObject = { firstName, lastName, userName, email, profile };
   const userIDObject = { userName, password: hashedPsswrd };
+  const userFavoritesObject = { userName, favorites: [] };
 
   const userDetailCreated = await userDetailsModel.create(userDetailsObject);
   const userIDCreated = await userIDModel.create(userIDObject);
+  const userFavoritesCreated = await userFavoritesModel.create(
+    userFavoritesObject
+  );
 
-  if (userDetailCreated && userIDCreated) {
+  if (userDetailCreated && userIDCreated && userFavoritesCreated) {
+    const accessToken = jwt.sign(
+      {
+        UserInfo: {
+          userName: userName,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    const refreshToken = jwt.sign(
+      {
+        userName: userName,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
     logEvents(`User created`);
-    res.sendStatus(201);
+    res.status(201).json({ accessToken });
   } else {
     res.sendStatus(400);
   }
