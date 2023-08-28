281909d0f50d1ec7d56bb5f84a2f72ab9de96ae1
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-08-28 10:23:15 +0530

    cors #01

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://treklicious.onrender.com'
+}));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.json());
